084b75fa4e78ba96e77e469bb1b46cc8151728ba
Horvath Mark <horvath.mark1@students.jedlik.eu>
2025-01-09 09:36:24 +0100

    login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { OffersComponent } from './offers/offers.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  {path: 'services', component: OffersComponent}
+  {path: 'services', component: OffersComponent},
+  {path: 'login', component: LoginComponent}
 ];
